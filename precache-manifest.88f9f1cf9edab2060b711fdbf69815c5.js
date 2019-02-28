63f5f02807d6508286db9d6a2ddce6dc17a3dc75
Lauri Rannu <lauri.rannu@gmail.com>
2019-02-28 13:15:48 +0200

    Updates

---
+++ precache-manifest.88f9f1cf9edab2060b711fdbf69815c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba6b288f50e68caaf14b42626f61cc2d",
+    "url": "/gotoAndBot/static/media/robot.ba6b288f.jpg"
+  },
+  {
+    "revision": "0b974ee68e3f71a3ad8b3e000fc1d06f",
+    "url": "/gotoAndBot/static/media/right-arrow.0b974ee6.svg"
+  },
+  {
+    "revision": "a84fffb0529dd3ffbd93",
+    "url": "/gotoAndBot/static/js/runtime~main.a84fffb0.js"
+  },
+  {
+    "revision": "4243d611713121c3a720",
+    "url": "/gotoAndBot/static/js/main.4243d611.chunk.js"
+  },
+  {
+    "revision": "3e2c906202040a538011",
+    "url": "/gotoAndBot/static/js/2.3e2c9062.chunk.js"
+  },
+  {
+    "revision": "4243d611713121c3a720",
+    "url": "/gotoAndBot/static/css/main.594e45b3.chunk.css"
+  },
+  {
+    "revision": "3ca45a3c4889c31b26c68e4d486f46ab",
+    "url": "/gotoAndBot/index.html"
+  }
+];
